0fffb6d4b3f90fc5778fe432c36d85070b9edff7
Mohamed-Amir kharmaz <amirpro@hotmail.fr>
2021-09-15 16:04:11 +0200

    Mise en place des routes Home, Hotels, Hotel Page, Error404

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import Home from './components/Home';
+import NavBar from './components/NavBar';
+import Hotels from './components/Hotels';
+import HotelPage from './components/HotelPage';
+import HotelCard from "./components/HotelCard";
+import HotelMap from "./components/HotelMap";
+import HotelMarker from "./components/HotelMarker";
+import CityCard from "./components/CityCard";
+import Error404 from './components/Error404';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>App</h1>
+        <NavBar/>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/hotels" component={Hotels} />
+          <Route path="/hotel" component={HotelPage} />
+          <Route path="*" component={Error404} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import {Link} from "react-router-dom";
+
 
 export default function NavBar() {
     return (
         <div>
-            <h1>Nav Bar</h1>
+            <nav>
+                <ul>
+                </ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/hotels">Hotels</Link></li>
+                    <li><Link to="/hotel">Hotel Page</Link></li>
+                    <li><Link to="*">Error 404</Link></li>
+            </nav>
         </div>
     )
 }
